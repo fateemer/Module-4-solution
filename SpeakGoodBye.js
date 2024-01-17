0906b419a10fcdf8423d9167979fd926c50ba90a
fateemer <156684479+fateemer@users.noreply.github.com>
2024-01-17 05:39:20 -0800

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,1 +1,11 @@
+// Create byeSpeaker object
+var byeSpeaker = {};
+
+// Attach the "speak" method to byeSpeaker
+byeSpeaker.speak = function(name) {
+  console.log("Good Bye " + name);
+};
+
+// Expose byeSpeaker to the global scope
+window.byeSpeaker = byeSpeaker;
 
