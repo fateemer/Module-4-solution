7e6b894ed10df960a28516d05b468916cca4136e
fateemer <156684479+fateemer@users.noreply.github.com>
2024-01-17 05:47:31 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+(function() {
+  // Your existing code for creating helloSpeaker and byeSpeaker objects
+
+  // Array of names
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  // Loop over the names array
+  for (var i = 0; i < names.length; i++) {
+    // Retrieve the first letter of the current name
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    // Compare the first letter to 'j' and call the appropriate speaker's 'speak' method
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]); // Call byeSpeaker's 'speak' method
+    } else {
+      helloSpeaker.speak(names[i]); // Call helloSpeaker's 'speak' method
+    }
+  }
+})();
 
