f38e372f1600c61e53269c1092be53b4050d82db
fateemer <156684479+fateemer@users.noreply.github.com>
2024-01-17 05:42:11 -0800

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,1 +1,11 @@
+// Create helloSpeaker object
+var helloSpeaker = {};
+
+// Attach the "speak" method to helloSpeaker
+helloSpeaker.speak = function(name) {
+  console.log("Hello " + name);
+};
+
+// Expose helloSpeaker to the global scope
+window.helloSpeaker = helloSpeaker;
 
